31246dba5b01ad2fecba4c583afc759c7c8863b2
luke <lukeaquillamuggerchua@gmail.com>
2024-11-09 13:05:17 +0800

    Exception Handling added for application found

--- src/main/java/com/example/TripServiceApplication.java
+++ src/main/java/com/example/TripServiceApplication.java
@@ -1,19 +1,26 @@
-package com.example;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package com.example;
+
 import com.example.Service.TripService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import java.io.IOException;
+
 
 @SpringBootApplication
 public class TripServiceApplication {
+
     public static void main(String[] args) throws InterruptedException, IOException {
         SpringApplication.run(TripServiceApplication.class, args);
-        Server server = ServerBuilder.forPort(5003).addService(new TripService()).build();
-        server.start();
-        server.awaitTermination();
+
+        // Start the gRPC server
+        Server server = ServerBuilder.forPort(5003)  // Listening on port 5003
+                .addService(new TripService())  // Add your TripService implementation
+                .build();
+        server.start();  // Start the server
+        System.out.println("Server started, waiting for termination...");
+        server.awaitTermination();  // Wait for server termination
     }
 }
+
