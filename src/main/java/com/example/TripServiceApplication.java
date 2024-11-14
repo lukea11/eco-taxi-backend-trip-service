e36d278d214ae1e2cba5d7797f594e9fbb38464f
roniathomas <roniathomas1409@gmail.com>
2024-11-15 01:50:16 +0800

    changed to auto start

--- src/main/java/com/example/TripServiceApplication.java
+++ src/main/java/com/example/TripServiceApplication.java
@@ -9,8 +9,21 @@ import io.grpc.ServerBuilder;
 @SpringBootApplication
 public class TripServiceApplication {
 
+
     public static void main(String[] args) throws InterruptedException{
         SpringApplication.run(TripServiceApplication.class, args);
 
+        // Start the gRPC server
+        Server server = ServerBuilder.forPort(5003).addService(new TripService()).build();
+
+        try {
+            server.start();
+            System.out.println("gRPC Trip Service server started on port" + server.getPort());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        server.awaitTermination();
     }
+
 }
